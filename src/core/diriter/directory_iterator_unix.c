a9b4fcd8fa11b9dd96b48befd3193d10ecf8b827
Davit Kalantaryan <davit.kalantaryan@desy.de>
2023-01-10 15:43:07 +0100

    linux test is ok

--- src/core/diriter/directory_iterator_unix.c
+++ src/core/diriter/directory_iterator_unix.c
@@ -21,40 +21,42 @@ CINTERNAL_BEGIN_C
 
 // https://stackoverflow.com/questions/61012825/how-to-identify-which-entries-are-files-and-which-are-directories-in-c
 
-static void WinDataToClbkData(DirIterFileData* a_pClbk, const struct dirent* a_pSysData)
+static void SysDataToClbkData(DirIterFileData* a_pClbk, const struct dirent* a_pSysData)
 {
-	a_pClbk->isDir = (a_pWinData->dwFileAttributes& FILE_ATTRIBUTE_DIRECTORY)? CINTERNAL_STATIC_CAST(uint64_t,1): CINTERNAL_STATIC_CAST(uint64_t, 0);
-	a_pClbk->fileAttributes = CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->dwFileAttributes);
-	a_pClbk->creationTimeIn100Ms = (CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->ftCreationTime.dwHighDateTime)<<32)& 
-		CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->ftCreationTime.dwLowDateTime);
-	a_pClbk->lastAccessTimeIn100Ms = (CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->ftLastAccessTime.dwHighDateTime) << 32) &
-		CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->ftLastAccessTime.dwLowDateTime);
-	a_pClbk->lastWriteTimeIn100Ms = (CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->ftLastWriteTime.dwHighDateTime) << 32) &
-		CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->ftLastWriteTime.dwLowDateTime);
-	a_pClbk->fileSize = (CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->nFileSizeHigh) << 32) &
-		CINTERNAL_STATIC_CAST(uint64_t, a_pWinData->nFileSizeLow);
-	a_pClbk->pFileName = a_pWinData->cFileName;
-	a_pClbk->pAlternateFileName = a_pWinData->cAlternateFileName;
+	a_pClbk->isDir = (a_pSysData->d_type == DT_DIR)? CINTERNAL_STATIC_CAST(uint64_t,1): CINTERNAL_STATIC_CAST(uint64_t, 0);
+	a_pClbk->fileAttributes = CINTERNAL_STATIC_CAST(uint64_t, a_pSysData->d_type);
+	a_pClbk->creationTimeIn100Ms = 0;
+	a_pClbk->lastAccessTimeIn100Ms = 0;
+	a_pClbk->lastWriteTimeIn100Ms = 0;
+	a_pClbk->fileSize = 0;
+	a_pClbk->pFileName = a_pSysData->d_name;
+	a_pClbk->pAlternateFileName = a_pSysData->d_name;
 }
 
 
 DIRITER_EXPORT void IterateOverDirectoryFiles(const char* a_sourceDirectory, TypeDirIterFunc a_callback, void* a_ud)
 {
-	int nReadDirResult;
-	DIR* pDir;
-	struct dirent* pResult;
-	struct dirent inpBuffer;
+	DIR* pDir = opendir(a_sourceDirectory);
 	
-	pDir = opendir(a_sourceDirectory);
-	
-	if(!pDir){return;}
-	
-	nReadDirResult = readdir_r(pDir,&inpBuffer,&pResult);
-	assert(nReadDirResult==0);
-	while(pResult){
-		if (pResult->d_name[0] == '.') {
-			if ((pResult->d_name[1] == 0) || ((pResult->d_name[1] == '.') && (pResult->d_name[2] == 0))) { continue; }
+	if(pDir){
+		int nReadDirResult, nReturnFromCallback;
+		struct dirent* pResult;
+		struct dirent inpBuffer;
+		DirIterFileData		aClbkData;
+		nReadDirResult = readdir_r(pDir,&inpBuffer,&pResult);
+		assert(nReadDirResult==0);
+		while(pResult){
+			if (pResult->d_name[0] == '.') {
+				if ((pResult->d_name[1] == 0) || ((pResult->d_name[1] == '.') && (pResult->d_name[2] == 0))) { continue; }
+			}
+			
+			SysDataToClbkData(&aClbkData, pResult);
+			
+			nReturnFromCallback = (*a_callback)(a_sourceDirectory,a_ud, &aClbkData);
+			if (nReturnFromCallback) { break; }
 		}
+		
+		closedir(pDir);
 	}
 }
 
